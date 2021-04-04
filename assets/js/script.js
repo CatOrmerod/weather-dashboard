cada016ac01a58bec8cf68249fccde6884130bc3
Cat Ormerod <catormerod@gmail.com>
2021-04-04 15:21:36 +1000

    initial script work done, nothing working yet

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const apiKey = "4972d2ac99ced943e674efe5c64859c4";
+const cityInputEl = $("#city-input").val();
+const searchInputEl = $("#search-button").val();
+const timeDisplayEl = $("#time-display").val();
+
+
+
+let todayDate = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+$("#time-display").html(todayDate);
+
+let searchBtn = $(".btn-success");
+    searchBtn.on("click", function () {
+        let city = $(this).siblings(".city-input").val().trim();
+    
+        localStorage.setItem(city);
+    })
+
+
+function getWeather() {
+    let city = $(".city-input").val();
+    let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+        fetch(apiURL);
+        console.log(apiURL);
+        
+}
+
+cityInputEl.addEventListener("click", function () {
+    getWeather();
+    
+});
+
+
